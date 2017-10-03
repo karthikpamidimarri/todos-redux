ae22452c64dd30fb3f3385e2728d14a69a56e091
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-03 22:16:04 +0530

    Step 3.5: As subscribe function called multiple times we used throttle function to set the interval to call only after one second.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
-import {loadState,saveState} from './localstorage';
+import { loadState, saveState } from './localstorage';
+import throttle from 'lodash/throttle';
 
 const persistedState = loadState();
 const store = createStore(
@@ -13,10 +14,10 @@ const store = createStore(
     persistedState
 );
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   saveState(
       { todos: store.getState().todos });
-});
+}, 1000));
 
 render(
   <Provider store={store}>
