a657dcb926ca61f3bbc1018474cdd6cf07bd09c1
karthikpamidimarri <karthikpamidimari@gmail.com>
2017-10-03 21:47:43 +0530

    Step 3: Adding browser local storage, to persist the state of the application in the localstorage using browser localStorage API.

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+import {loadState,saveState} from './localstorage';
 
-const persistedState = ''; /*   */
+const persistedState = loadState();
 const store = createStore(
     todoApp,
     persistedState
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
 render(
   <Provider store={store}>
     <App />
